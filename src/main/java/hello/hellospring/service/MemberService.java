321478ed10f0d456344609c0eff8e834ffb8297c
rapael3020 <rapael3020@gmail.com>
2021-09-02 10:40:18 +0900

    JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,15 +2,13 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //testcase 알트엔터
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
